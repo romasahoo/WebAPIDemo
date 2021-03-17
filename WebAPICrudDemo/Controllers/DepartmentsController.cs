29ba45da7623a5b5e63d9b206589c193fbca872f
romasahoo <roma.sahoo.one@gmail.com>
2021-03-17 16:44:56 +0530

    test

--- WebAPICrudDemo/Controllers/DepartmentsController.cs
+++ WebAPICrudDemo/Controllers/DepartmentsController.cs
@@ -43,7 +43,6 @@ namespace WebAPICrudDemo.Controllers
         }
 
         // PUT: api/Departments/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutDepartment(int id, Department department)
         {
